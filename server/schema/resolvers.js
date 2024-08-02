54117585a90755952ecd8b49b1c49dd50b1308c2
Thannia Hernandez <thaherna1@gmail.com>
2024-08-02 12:06:24 -0500

    was able to make API Key work

--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -1,20 +1,26 @@
+require('dotenv').config();
+// console.log(`API Key: ${process.env.SPOONACULAR_API_KEY}`); // Log the API key to check if it is being loaded
 const { AuthenticationError } = require('apollo-server-express');
 const { Review, User } = require('../models');
 const { signToken } = require('../utils/auth');
 const fetch = require('node-fetch');
-
-require('dotenv').config();
 
 const fetchRecipes = async (query) => {
   const apiKey = process.env.SPOONACULAR_API_KEY;
   const apiUrl = `https://api.spoonacular.com/recipes/complexSearch?query=${query}&apiKey=${apiKey}&number=10`;
 
   try {
+    console.log(`Fetching recipes from API: ${apiUrl}`);
     const response = await fetch(apiUrl);
+    console.log(`API response status: ${response.status}`);
+
     if (!response.ok) {
       throw new Error(`Error fetching recipes: ${response.statusText}`);
     }
+
     const data = await response.json();
+    console.log(`API response data: ${JSON.stringify(data)}`);
+
     return data.results;
   } catch (error) {
     console.error(error);
@@ -22,57 +28,52 @@ const fetchRecipes = async (query) => {
   }
 };
 
-
 const resolvers = {
-    Query: {
-        // Fetch a user by ID
-        user: async (parent, { id }) => {
-          return User.findById(id);
-        },
-        // Fetch all recipes from an external API
-        recipes: async (parent, {query}) => {
-          return fetchRecipes(query)
-        },
-        // Fetch reviews for a specific recipe
-        reviews: async (parent, { recipeId }) => {
-          return Review.find({ recipeId });
-        }
-      },
-      // Mutation: {
-      //   // User sign-up
-      //   addUser: async (parent, { username, email, password }) => {
-      //     const user = await User.create({ username, email, password });
-      //     const token = signToken(user);
-      //     return { token, user };
-      //   },
-      //   // User login
-      //   login: async (parent, { email, password }) => {
-      //     const user = await User.findOne({ email });
-      //     if (!user) {
-      //       throw new AuthenticationError('Incorrect credentials');
-      //     }
-    
-      //     const correctPw = await user.isCorrectPassword(password);
-      //     if (!correctPw) {
-      //       throw new AuthenticationError('Incorrect credentials');
-      //     }
-    
-      //     const token = signToken(user);
-      //     return { token, user };
-      //   },
-      //   addReview: async (parent, { title, content }, context) => {
-      //     if (context.user) {
-      //       const review = await Review.create({
-      //         title,
-      //         content,
-      //         author: context.user._id,
-      //       });
-    
-      //       return review;
-      //     }
-      //     throw new AuthenticationError('Not logged in');
-      //   }
-      // }
-    };
+  Query: {
+    user: async (parent, { id }) => {
+      return User.findById(id);
+    },
+    recipes: async (parent, { query }) => {
+      return fetchRecipes(query);
+    },
+    reviews: async (parent, { recipeId }) => {
+      return Review.find({ recipeId });
+    }
+  },
+  // Mutation: {
+  //   addUser: async (parent, { username, email, password }) => {
+  //     const user = await User.create({ username, email, password });
+  //     const token = signToken(user);
+  //     return { token, user };
+  //   },
+  //   login: async (parent, { email, password }) => {
+  //     const user = await User.findOne({ email });
+  //     if (!user) {
+  //       throw new AuthenticationError('Incorrect credentials');
+  //     }
+
+  //     const correctPw = await user.isCorrectPassword(password);
+  //     if (!correctPw) {
+  //       throw new AuthenticationError('Incorrect credentials');
+  //     }
+
+  //     const token = signToken(user);
+  //     return { token, user };
+  //   },
+  //   addReview: async (parent, { recipeId, reviewText, rating }, context) => {
+  //     if (context.user) {
+  //       const review = await Review.create({
+  //         recipeId,
+  //         reviewText,
+  //         rating,
+  //         username: context.user.username
+  //       });
+
+  //       return review;
+  //     }
+  //     throw new AuthenticationError('Not logged in');
+  //   }
+  // }
+};
 
 module.exports = resolvers;
