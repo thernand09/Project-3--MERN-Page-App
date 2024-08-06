516896096fd3e406a84aeb7926408c4050463fd3
dannywashere <dannylkramer@icloud.com>
2024-08-05 20:25:26 -0500

    auth, components, and pages framework

---
+++ client/src/main.jsx
@@ -0,0 +1,10 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import App from './App';
+// import './styles/index.css'; // Import global styles if you have any
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// );
---
+++ client/src/pages/SearchResults.jsx
@@ -0,0 +1,2 @@
+// this page will display the list of recipes associated with the user's search
+// need to import 
